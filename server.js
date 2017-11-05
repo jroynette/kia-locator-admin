091d536bf9a8805875a091a7a4e79069ac4e307c
BLA <blahouaoula@gmail.com>
2017-11-05 19:44:03 +0100

    chore: configure git for heroku deploy

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var express  = require('express');
-var path  = require('path');
-var app      = express();                               // create our app w/ express
-
-app.use(express.static(path.resolve(__dirname, "www")));
+var express = require('express'),
+app = express();
+app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
-  console.log('listening to port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
